480ae2700529826fea311f491d46c9f95ef44159
shahiq63 <syed.shahiq@square63.com>
2018-11-19 17:25:25 +0500

    header and footer separated

--- wp-content/themes/listing/includes/front/setup.php
+++ wp-content/themes/listing/includes/front/setup.php
@@ -2,5 +2,5 @@
 
 function ju_setup_theme() {
   add_theme_support('menus');
-   register_nav_menu('primary',__('Primary Menu','Theme Test Menu'));
+  register_nav_menu('primary',__('Primary Menu','Theme Test Menu'));
 }
