94efc6f5ab5a8f1dd1b1ce8240fc1b75450520c1
shahiq63 <syed.shahiq@square63.com>
2018-11-26 13:45:27 +0500

    Social Links Added

--- wp-content/themes/listing/functions.php
+++ wp-content/themes/listing/functions.php
@@ -7,9 +7,11 @@
 include(get_template_directory(). '/includes/front/enqueue.php');
 include(get_template_directory(). '/includes/front/setup.php');
 include(get_template_directory(). '/includes/front/widgets.php');
+include(get_template_directory(). '/includes/front/theme-customizer.php');
 //Hooks
 
 add_action('wp_enqueue_scripts','ju_enqueue');
 add_action('after_setup_theme','ju_setup_theme');
 add_action('widgets_init','ju_widgets');
+add_action('customize_register','ju_customize_register');
 //ShortCodes
---
+++ wp-content/themes/listing/includes/front/theme-customizer.php
@@ -0,0 +1,78 @@
+<?php
+
+function ju_customize_register($wp_customize) {
+  $wp_customize->add_setting('ju_facebook_handle',array(
+    'default'=>''
+  ));
+  $wp_customize->add_setting( 'ju_twitter_handle', array(
+    'default'                   =>  '',
+  ));
+  $wp_customize->add_setting( 'ju_instagram_handle', array(
+    'default'                   =>  '',
+  ));
+  $wp_customize->add_setting( 'ju_phone_handle', array(
+    'default'                   =>  '',
+  ));
+  $wp_customize->add_setting( 'ju_email_handle', array(
+    'default'                   =>  '',
+  ));
+  $wp_customize->add_section('ju_social_section',array(
+    'title'=>__('Social Links Settings','Test-wp'),
+    'priority'=>30
+  ));
+  $wp_customize->add_control(
+    new WP_Customize_Control(
+        $wp_customize,
+        'ju_social_facebook_input',
+        array(
+            'label'          => __( 'Facebook Link here', 'listing' ),
+            'section'        => 'ju_social_section',
+            'settings'       => 'ju_facebook_handle',
+            'type'           => 'text',
+        )
+    ));
+  $wp_customize->add_control(
+    new WP_Customize_Control(
+        $wp_customize,
+        'ju_social_Twitter_input',
+        array(
+            'label'          => __( 'Twitter Link here', 'listing' ),
+            'section'        => 'ju_social_section',
+            'settings'       => 'ju_twitter_handle',
+            'type'           => 'text',
+        )
+    ));
+  $wp_customize->add_control(
+    new WP_Customize_Control(
+        $wp_customize,
+        'ju_social_Instagram_input',
+        array(
+            'label'          => __( 'Instagram Link here', 'listing' ),
+            'section'        => 'ju_social_section',
+            'settings'       => 'ju_instagram_handle',
+            'type'           => 'text',
+        )
+    ));
+    $wp_customize->add_control(
+    new WP_Customize_Control(
+        $wp_customize,
+        'ju_social_Phone_input',
+        array(
+            'label'          => __( 'Phone Number here', 'listing' ),
+            'section'        => 'ju_social_section',
+            'settings'       => 'ju_phone_handle',
+            'type'           => 'text',
+        )
+    ));
+      $wp_customize->add_control(
+    new WP_Customize_Control(
+        $wp_customize,
+        'ju_social_Email_input',
+        array(
+            'label'          => __( 'Email Link here', 'listing' ),
+            'section'        => 'ju_social_section',
+            'settings'       => 'ju_email_handle',
+            'type'           => 'text',
+        )
+    ));
+}
