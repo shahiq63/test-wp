e9f1f702777d303b0eaa59fa4c3619afd61b90b3
Syed Shahiq <syed.shahiq@square63.com>
2018-12-03 13:53:58 +0500

    Ratings and Ratings count added in admin panel

---
+++ wp-content/plugins/recipe/includes/admin/columns.php
@@ -0,0 +1,30 @@
+<?php
+
+function r_add_new_recipe_column( $columns ) {
+  var_dump("Sssss");
+  $new_columns =array();
+  $new_columns['cb']           = '<input type="checkbox" />';
+  $new_columns['title']        = __('Title','recipe');
+  $new_columns['author']       = __('Author','recipe');
+  $new_columns['categories']   = __('Categories','recipe');
+  $new_columns['count']        = __('Ratings Count','recipe');
+  $new_columns['ratings']      = __('Average Ratings','recipe');
+  $new_columns['date']         = __('Date','recipe');
+
+  return $new_columns;
+}
+function r_manage_recipe_columns( $column , $post_id ) {
+  switch ($column) {
+    case 'count':
+      $recipe_data = get_post_meta($post_id,'recipe_data',true);
+      echo $recipe_data['rating_count'];
+      break;
+    case 'ratings':
+      $recipe_data = get_post_meta($post_id,'recipe_data',true);
+      echo $recipe_data['rating'];
+      break;
+    default:
+      break;
+  }
+
+}
