dd1aca5694cd45520f4b0eaef7118d83d941df0b
Syed Shahiq <syed.shahiq@square63.com>
2018-12-05 15:49:28 +0500

    Checks added

--- wp-content/plugins/recipe/includes/process/rate-recipe.php
+++ wp-content/plugins/recipe/includes/process/rate-recipe.php
@@ -4,7 +4,12 @@ function r_rate_recipe() {
   global $wpdb;
 
   $output = array('status'=> 1);
+  $recipe_opts            =   get_option( 'r_opts' );
 
+  if( !is_user_logged_in() && $recipe_opts['rating_login_required'] == 2 ){
+    wp_send_json( $output );
+  }
+  
   $post_id = absint($_POST['rid']);
   $rating  = round($_POST['rating'],1);
   $user_IP = $_SERVER['REMOTE_ADDR'];
--- wp-content/plugins/recipe/includes/shortcodes/creater.php
+++ wp-content/plugins/recipe/includes/shortcodes/creater.php
@@ -1,6 +1,12 @@
 <?php
 
 function r_recipe_creator_shortcode () {
+  $recipe_opts            =   get_option( 'r_opts' );
+
+  if( !is_user_logged_in() && $recipe_opts['recipe_submission_login_required'] == 2 ){
+    return 'You must be logged in to submit a recipe.';
+  }
+  
   $creatorHTML = file_get_contents('creater-template.php',true);
 
   $editorHTML = r_generate_content_editor();
